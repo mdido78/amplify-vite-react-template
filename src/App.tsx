7010ff181341f626a34d392def80dcf3e8b1be66
Marco Di Domenico <marco.didomenico@xero.com>
2025-03-14 13:35:42 -0600

    Add authentication and Xero token retrieval functionality
    - Integrated AWS Amplify Authenticator for user authentication.
    - Implemented functionality to retrieve and display access tokens.
    - Updated UI to reflect changes, including new buttons and text areas.
    - Enhanced CSS for better styling of text areas and buttons.

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,49 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
+import { useAuthenticator } from '@aws-amplify/ui-react';
 import { generateClient } from "aws-amplify/data";
 
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
+  const { signOut } = useAuthenticator();
+  const [myToken, setMyToken] = useState("");
+  const [xeroToken, setXeroToken] = useState("");
+  
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
+    setMyToken(window.localStorage.getItem("CognitoIdentityServiceProvider.3j91ble2u7jm28ul214k0esaa5.c43864d8-3031-700b-dd41-96bcf99c965c.accessToken") ?? "");
   }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+  function GetXeroToken() {
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + myToken);
+    
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+      mode: "cors",
+      credentials: "include" 
+    };
+    
+    fetch("https://d7evg95uyk.execute-api.us-east-1.amazonaws.com/Prod/token", requestOptions)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.text();
+      })
+      .then((result) => setXeroToken(result))
+      .catch((error) => console.error(error));
   }
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
+    <main>      
+      <h1>My Access Token</h1>
+      <textarea readOnly value={myToken} />
+      <button onClick={GetXeroToken}>Get Xero Access Token</button>            
+      <textarea readOnly value={xeroToken} />
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
